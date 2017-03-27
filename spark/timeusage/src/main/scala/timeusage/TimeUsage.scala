eca86868fa8d84d7a23bdcf76e33f1863920f486
Brian Murphy <btm@integrichain.com>
2017-03-27 16:22:05 -0400

    TimeUsage::dfSchema

--- spark/timeusage/src/main/scala/timeusage/TimeUsage.scala
+++ spark/timeusage/src/main/scala/timeusage/TimeUsage.scala
@@ -62,8 +62,11 @@ object TimeUsage {
     *         have type Double. None of the fields are nullable.
     * @param columnNames Column names of the DataFrame
     */
-  def dfSchema(columnNames: List[String]): StructType =
-    ???
+  def dfSchema(columnNames: List[String]): StructType = {
+    val x = StructField(columnNames.head, StringType, nullable = true)
+    val xs = columnNames.tail.map(fld => StructField(fld, DoubleType, nullable = true))
+    StructType(x :: xs)
+  }
 
 
   /** @return An RDD Row compatible with the schema produced by `dfSchema`
